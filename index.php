778c5eaca2bb860f378bb352160566527d12e9e2
vbloke <vbloke@gmail.com>
2015-05-06 18:01:38 +0100

    fixes

--- index.php
+++ index.php
@@ -48,13 +48,12 @@
 					<?php
 					if (have_posts()) : while (have_posts()) : the_post();
 					$thumbLarge = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbLarge' ); $urlLarge = $thumbLarge['0'];
-					$thumbSquare = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbSquare' ); $urlSquare = $thumbSquare['0'];
 					?>
 
 					<?php global $post; $double = get_post_meta( $post->ID, '_wl_portfolio_double', true ); if( $double == 'on' ) : ?>
 					<div <?php post_class('item triple');?> style="background-image:url(<?php echo $urlLarge; ?>);" itemscope itemtype="http://schema.org/Article">
 					<?php else : ?>
-					<div <?php post_class('item double');?> style="background-image:url(<?php echo $urlSquare; ?>);" itemscope itemtype="http://schema.org/Article">
+					<div <?php post_class('item double');?> style="background-image:url(<?php echo $urlLarge; ?>);" itemscope itemtype="http://schema.org/Article">
 					<?php endif; ?>
 					<meta itemprop="keywords" content="<?php $keywords = get_post_meta( get_the_ID(), '_wl_seo_keywords', true ); echo $keywords; ?>" />
 					<meta itemprop="description" content="<?php $description = get_post_meta( get_the_ID(), '_wl_seo_desc', true ); echo $description; ?>" />
